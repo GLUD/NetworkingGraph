341415fb36b479e9a771fb495a9845a21073da6e
MauricioAcosta <andres.taamap@gmail.com>
2018-03-28 04:44:25 -0500

    Cuerpo del proyecto, falta mejorar

---
+++ src/iniciar.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-sm-6">
+      <br>
+      <h1>Trazado de rutas remotamente</h1>
+      <div class="col-sm-3">
+        <button type="button" class="btn btn-primary">Ecuador</button>
+      </div>
+      <div class="col-sm-3">
+        <button type="button" class="btn btn-primary">Estados Unidos</button>
+      </div>
+      <div class="col-sm-3">
+        <button type="button" class="btn btn-primary">Sur America</button>
+      </div>
+      <div class="col-sm-3">
+        <button type="button" class="btn btn-primary">Oceania</button>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <br>
+      <h1>Trazado de rutas localmente</h1>
+      <div class="col-sm-3">
+        <button type="button" class="btn btn-primary">local</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'iniciar',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+// import Iniciar from './iniciar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
@@ -10,3 +11,7 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+// new Vue({
+//   el: '#iniciar',
+//   render: h => h(Iniciar)
+// })
