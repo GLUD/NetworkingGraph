a5d248dd0386856b31bfa8049f7f124d1470a274
MauricioAcosta <andres.taamap@gmail.com>
2018-03-31 13:07:31 -0500

    documentación del proyecto.

--- src/components/screens/00-networking/networking-01.vue
+++ src/components/screens/00-networking/networking-01.vue
@@ -1,48 +1,51 @@
 <template>
 <div class="container">
+  <router-link aria-label="Página anterior" class="rout-bot bot-left hvr-grow" to="/"></router-link>
   <div class="row text-center">
-    <div class="col-sm-6">
-      <h1>Remotamente</h1>
-      <div class="col-sm-12">
-        <button type="button" class="btn btn-primary btn-sm btn-block" @click="servidor=true, ecuador()">Ecuador</button>
-        <br>
-      </div>
-      <div class="col-sm-12">
-        <button type="button" class="btn btn-primary btn-sm btn-block" @click="servidor=true, EEUU()">Estados Unidos</button>
-        <br>
-      </div>
-      <div class="col-sm-12">
-        <button type="button" class="btn btn-primary btn-sm btn-block" @click="servidor=true, SurAmerica()">Sur America</button>
-        <br>
-      </div>
-      <div class="col-sm-12">
-        <button type="button" class="btn btn-primary btn-sm btn-block" @click="servidor=true, Oceania()">Oceania</button>
-        <br>
-      </div>
-    </div>
-    <div class="col-sm-6">
-      <h1>Localmente</h1>
-      <div class="col-sm-12">
-        <button type="button" class="btn btn-primary btn-sm btn-block" @click="servidor=true, local()">local</button>
-        <br>
-      </div>
-    </div>
-    <div class="col-sm-12" v-if="servidor">
+    <div class="col-sm-12">
       <h4>Seleccioné DNS</h4>
       <br>
       <form class="text-center">
         <label class="col-sm-3 radio-inline">
-            <input type="radio" name="google" value="google.com" v-model="GuardarDato">1. Google.com
+            <input type="radio" name="google" value="google.com" @click="servidor=true" v-model="GuardarDato">1. Google.com
         </label>
         <label class="col-sm-3 radio-inline">
-            <input type="radio" name="youtube" value="youtube.com" v-model="GuardarDato">2. Youtube.com
+            <input type="radio" name="youtube" value="youtube.com" @click="servidor=true" v-model="GuardarDato">2. Youtube.com
           </label>
         <label class="col-sm-3 radio-inline">
-            <input type="radio" name="facebook" value="facebook.com" v-model="GuardarDato">3. Facebook.com
+            <input type="radio" name="facebook" value="facebook.com" @click="servidor=true" v-model="GuardarDato">3. Facebook.com
           </label>
       </form>
     </div>
+    <div class="col-sm-6" v-if="servidor">
+      <h1>Remotamente</h1>
+      <div class="col-sm-12">
+        <button type="button" class="btn btn-primary btn-sm btn-block" @click="ecuador()">Ecuador</button>
+        <br>
+      </div>
+      <div class="col-sm-12">
+        <button type="button" class="btn btn-primary btn-sm btn-block" @click="EEUU()">Estados Unidos</button>
+        <br>
+      </div>
+      <div class="col-sm-12">
+        <button type="button" class="btn btn-primary btn-sm btn-block" @click="SurAmerica()">Sur America</button>
+        <br>
+      </div>
+      <div class="col-sm-12">
+        <button type="button" class="btn btn-primary btn-sm btn-block" @click="Oceania()">Oceania</button>
+        <br>
+      </div>
+    </div>
+    <div class="col-sm-6" v-if="servidor">
+      <h1>Localmente</h1>
+      <div class="col-sm-12">
+        <button type="button" class="btn btn-primary btn-sm btn-block" @click="local()">local</button>
+        <br>
+      </div>
+    </div>
   </div>
+  <!-- Ya guardo el dato -->
+  {{this.$store.state.pregunta.direccion}}
 </div>
 </template>
 <!-- http://gcoch.github.io/D3-tutorial/asociar-datos.html -->
@@ -52,23 +55,24 @@ export default {
   data() {
     return {
       servidor: false,
+      dns: "",
     };
   },
   methods: {
     ecuador() {
-
+      this.dns = this.$store.state.pregunta.direccion
     },
     EEUU() {
-
+      this.dns = this.$store.state.pregunta.direccion
     },
     SurAmerica() {
-
+      this.dns = this.$store.state.pregunta.direccion
     },
     Oceania() {
-
+      this.dns = this.$store.state.pregunta.direccion
     },
     local() {
-
+      this.dns = this.$store.state.pregunta.direccion
     },
     preguntas() {
       console.log(this.$store.state.pregunta.direccion);
